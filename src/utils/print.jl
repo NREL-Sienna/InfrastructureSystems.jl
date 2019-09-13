17097d9320269bebd0e6d133356bbad62db17103
Daniel Thom <daniel.thom@nrel.gov>
2019-09-13 15:39:22 -0600

    Limit number of components and forecasts to be printed.

--- src/utils/print.jl
+++ src/utils/print.jl
@@ -1,63 +1,27 @@
 
-function Base.show(io::IO, components::Components)
-    for component in iterate_components(components)
-        show(io, component)
-        println(io)
-    end
-end
+const MAX_SHOW_COMPONENTS = 10
+const MAX_SHOW_FORECASTS = 10
+const MAX_SHOW_FORECAST_INITIAL_TIMES = 1
+
 
 function Base.summary(io::IO, components::Components)
     print(io, "$(typeof(components))")
 end
 
-function create_components_df(components::Components)
-    counts = Dict{String, Int}()
-    rows = []
-
-    for (subtype, values) in components.data
-        type_str = strip_module_names(string(subtype))
-        counts[type_str] = length(values)
-        parents = [strip_module_names(string(x)) for x in supertypes(subtype)]
-        row = (ConcreteType=type_str,
-               SuperTypes=join(parents, " <: "),
-               Count=length(values))
-        push!(rows, row)
-    end
-
-    sort!(rows, by = x -> x.ConcreteType)
-
-    return DataFrames.DataFrame(rows)
-end
-
-function create_forecasts_df(forecasts::Forecasts)
-    initial_times = _get_forecast_initial_times(forecasts.data)
-    dfs = Vector{DataFrames.DataFrame}()
-
-    for initial_time in initial_times
-        counts = Dict{String, Int}()
-        rows = []
-
-        for (key, values) in forecasts.data
-            if key.initial_time != initial_time
-                continue
-            end
-
-            type_str = strip_module_names(string(key.forecast_type))
-            counts[type_str] = length(values)
-            parents = [strip_module_names(string(x)) for x in supertypes(key.forecast_type)]
-            row = (ConcreteType=type_str,
-                   SuperTypes=join(parents, " <: "),
-                   Count=length(values))
-            push!(rows, row)
+function Base.show(io::IO, components::Components)
+    i = 1
+    for component in iterate_components(components)
+        if i <= MAX_SHOW_COMPONENTS
+            show(io, component)
+            println(io)
         end
-
-        sort!(rows, by = x -> x.ConcreteType)
-
-        df = DataFrames.DataFrame(rows)
-        push!(dfs, df)
+        i += 1
     end
 
-    return initial_times, dfs
+    if i > MAX_SHOW_COMPONENTS
+        num = i - MAX_SHOW_COMPONENTS
+        println(io, "\n***Omitted $num components***\n")
+    end
 end
 
 function Base.show(io::IO, ::MIME"text/plain", components::Components)
@@ -86,9 +50,18 @@ function Base.summary(io::IO, forecasts::Forecasts)
 end
 
 function Base.show(io::IO, forecasts::Forecasts)
+    i = 1
     for forecast in iterate_forecasts(forecasts)
-        show(io, forecast)
-        println(io)
+        if i <= MAX_SHOW_FORECASTS
+            show(io, forecast)
+            println(io)
+        end
+        i += 1
+    end
+
+    if i > MAX_SHOW_FORECASTS
+        num = i - MAX_SHOW_FORECASTS
+        println(io, "\n***Omitted $num forecasts***\n")
     end
 end
 
@@ -106,6 +79,11 @@ function Base.show(io::IO, ::MIME"text/plain", forecasts::Forecasts)
         println(io, "---------------------------------")
         show(io, df)
     end
+
+    if length(initial_times) > MAX_SHOW_FORECAST_INITIAL_TIMES
+        num = length(initial_times) - MAX_SHOW_FORECAST_INITIAL_TIMES
+        println(io, "\n\n***Omitted tables for $num initial times***\n")
+    end
 end
 
 function Base.show(io::IO, ::MIME"text/html", forecasts::Forecasts)
@@ -120,6 +98,11 @@ function Base.show(io::IO, ::MIME"text/html", forecasts::Forecasts)
         withenv("LINES" => 100, "COLUMNS" => 200) do
             show(io, MIME"text/html"(), df)
         end
+    end
+
+    if length(initial_times) > MAX_SHOW_FORECAST_INITIAL_TIMES
+        num = length(initial_times) - MAX_SHOW_FORECAST_INITIAL_TIMES
+        println(io, "<p><b>Omitted tables for $num initial times</b></p>")
     end
 end
 
@@ -159,3 +142,58 @@ function Base.show(io::IO, ::MIME"text/plain", ist::InfrastructureSystemsType)
         print(io, "\n   ", name, ": ", val)
     end
 end
+
+function create_components_df(components::Components)
+    counts = Dict{String, Int}()
+    rows = []
+
+    for (subtype, values) in components.data
+        type_str = strip_module_names(string(subtype))
+        counts[type_str] = length(values)
+        parents = [strip_module_names(string(x)) for x in supertypes(subtype)]
+        row = (ConcreteType=type_str,
+               SuperTypes=join(parents, " <: "),
+               Count=length(values))
+        push!(rows, row)
+    end
+
+    sort!(rows, by = x -> x.ConcreteType)
+
+    return DataFrames.DataFrame(rows)
+end
+
+function create_forecasts_df(forecasts::Forecasts)
+    initial_times = _get_forecast_initial_times(forecasts.data)
+    dfs = Vector{DataFrames.DataFrame}()
+
+    for (i, initial_time) in enumerate(initial_times)
+        if i > MAX_SHOW_FORECAST_INITIAL_TIMES
+            break
+        end
+        counts = Dict{String, Int}()
+        rows = []
+
+        for (key, values) in forecasts.data
+            if key.initial_time != initial_time
+                continue
+            end
+
+            type_str = strip_module_names(string(key.forecast_type))
+            counts[type_str] = length(values)
+            parents = [strip_module_names(string(x)) for x in supertypes(key.forecast_type)]
+            row = (ConcreteType=type_str,
+                   SuperTypes=join(parents, " <: "),
+                   Count=length(values))
+            push!(rows, row)
+        end
+
+        sort!(rows, by = x -> x.ConcreteType)
+
+        df = DataFrames.DataFrame(rows)
+        push!(dfs, df)
+    end
+
+    return initial_times, dfs
+end
+
+
