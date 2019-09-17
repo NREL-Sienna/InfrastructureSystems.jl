9fc4987bb47ec9c2eb11fa0069d05f063f82e369
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-17 17:29:03 -0600

    remove exports

--- src/InfrastructureSystems.jl
+++ src/InfrastructureSystems.jl
@@ -7,63 +7,13 @@ import JSON
 import JSON2
 import TimeSeries
 
-export Components
-export FlattenIteratorWrapper
-export Forecasts
-export InfrastructureSystemsInternal
-export InfrastructureSystemsType
-export SystemData
-export TimeseriesFileMetadata
-
-export Forecast
-export Deterministic
-export Probabilistic
-export ScenarioBased
-
-export get_limits
-export get_name
-export open_file_logger
-export validate_struct
-
-# Components functions
-export iterate_components
-export add_component!
-export remove_components!
-export remove_component!
-export get_component
-export get_components_by_name
-export get_components
-
-# Forecasts functions
-export add_forecasts!
-export add_forecast!
-export remove_forecast!
-export clear_forecasts!
-export get_forecast_initial_times
-export get_forecasts
-export get_forecasts_horizon
-export get_forecasts_initial_time
-export get_forecasts_interval
-export get_forecasts_resolution
-export get_forecast_component_name
-export get_forecast_value
-export get_timeseries
-export iterate_forecasts
-export make_forecasts
-export split_forecasts!
-export read_timeseries_metadata
-
-export runtests
-
-export DataFormatError
-export InvalidRange
-export InvalidValue
+# IS should not export any function since it can have name clashes with other packages.
+# Do not add export statements.
 
 # Every subtype must implement InfrastructureSystems.get_name() or have a field called name.
 abstract type InfrastructureSystemsType end
 
 get_name(value::InfrastructureSystemsType) = value.name
-
 
 include("common.jl")
 include("internal.jl")
