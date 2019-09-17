85f9882aa47116c3028aa2a90b3bd773e860ec27
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-17 17:29:11 -0600

    update tests

--- test/test_components.jl
+++ test/test_components.jl
@@ -1,97 +1,97 @@
 
 @testset "Test add_component" begin
-    container = Components()
+    container = IS.Components()
 
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
     @test length(container.data) == 1
     @test length(container.data[TestComponent]) == 1
 
-    @test_throws ArgumentError add_component!(container, component)
+    @test_throws ArgumentError IS.add_component!(container, component)
 
     struct BadComponent
         name::AbstractString
         val::Int
     end
 
-    container = Components()
+    container = IS.Components()
     component = BadComponent("component1", 5)
-    @test_throws MethodError add_component!(container, component)
+    @test_throws MethodError IS.add_component!(container, component)
 end
 
 @testset "Test remove_component" begin
-    container = Components()
+    container = IS.Components()
 
     component = TestComponent("component1", 5)
-    add_component!(container, component)
-    components = get_components(TestComponent, container)
+    IS.add_component!(container, component)
+    components = IS.get_components(TestComponent, container)
     @test length(components) == 1
 
-    remove_component!(container, component)
-    components = get_components(TestComponent, container)
+    IS.remove_component!(container, component)
+    components = IS.get_components(TestComponent, container)
     @test length(components) == 0
 end
 
-@testset "Test get_components" begin
-    container = Components()
+@testset "Test IS.get_components" begin
+    container = IS.Components()
 
     # empty
-    components = get_components(TestComponent, container)
+    components = IS.get_components(TestComponent, container)
     @test length(components) == 0
 
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
 
     # by abstract type
-    components = get_components(InfrastructureSystemsType, container)
+    components = IS.get_components(IS.InfrastructureSystemsType, container)
     @test length(components) == 1
 
     # by concrete type
-    components = get_components(TestComponent, container)
+    components = IS.get_components(TestComponent, container)
     @test length(components) == 1
 
 end
 
-@testset "Test get_component" begin
-    container = Components()
+@testset "Test IS.get_component" begin
+    container = IS.Components()
 
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
 
-    component = get_component(TestComponent, container, "component1")
+    component = IS.get_component(TestComponent, container, "component1")
     @test component.name == "component1"
     @test component.val == 5
 
-    @test_throws ArgumentError get_component(InfrastructureSystemsType, container, "component1")
+    @test_throws ArgumentError IS.get_component(IS.InfrastructureSystemsType, container, "component1")
 end
 
-@testset "Test get_components_by_name" begin
-    container = Components()
+@testset "Test IS.get_components_by_name" begin
+    container = IS.Components()
 
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
 
-    components = get_components_by_name(InfrastructureSystemsType, container, "component1")
+    components = IS.get_components_by_name(IS.InfrastructureSystemsType, container, "component1")
     @test length(components) == 1
     @test component.name == "component1"
     @test component.val == 5
 end
 
-@testset "Test iterate_components" begin
-    container = Components()
+@testset "Test IS.iterate_components" begin
+    container = IS.Components()
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
 
     i = 0
-    for component in iterate_components(container)
+    for component in IS.iterate_components(container)
         i += 1
     end
     @test i == 1
 end
 
 @testset "Summarize components" begin
-    container = Components()
+    container = IS.Components()
     component = TestComponent("component1", 5)
-    add_component!(container, component)
+    IS.add_component!(container, component)
     summary(devnull, container)
 end
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,5 +1,5 @@
 
 @testset "Test utility functions" begin
-    concrete_types = IS.get_all_concrete_subtypes(InfrastructureSystemsType)
+    concrete_types = IS.get_all_concrete_subtypes(IS.InfrastructureSystemsType)
     @test length([x for x in concrete_types if isconcretetype(x)]) == length(concrete_types)
 end
