a10ed0812c5eeec473b42aefb7df90e031f00abd
Daniel Thom <daniel.thom@nrel.gov>
2019-10-22 16:31:12 -0600

    Fixed struct reference

--- test/test_hdf5_time_series_storage.jl
+++ test/test_hdf5_time_series_storage.jl
@@ -1,6 +1,6 @@
 
 @testset "Test automatic file deletion" begin
-    storage = Hdf5TimeSeriesStorage()
+    storage = IS.Hdf5TimeSeriesStorage()
     filename = storage.file_path
     @test isfile(filename)
     storage = nothing
