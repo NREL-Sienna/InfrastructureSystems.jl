5779e9fb56f12bf8012bc05210091d56042f3470
Lily Hanig <lanig12345@gmail.com>
2019-12-16 14:49:03 -0700

    adding dateperiod conversion method

--- src/units.jl
+++ src/units.jl
@@ -1,1 +1,2 @@
 time_period_conversion(time_period::Dates.TimePeriod) = convert(Dates.Millisecond, time_period)
+time_period_conversion(time_period::Dates.DatePeriod) = convert(Dates.Millisecond, time_period)
