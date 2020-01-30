59d2423b0589d8386b243379a23a0f19a1a8faeb
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 12:52:37 -0800

    add github actions

---
+++ .github/workflows/formatter_code.jl
@@ -0,0 +1,14 @@
+main_paths = ["./src", "./test"]
+for main_path in main_paths
+    for folder in readdir(main_path)
+        folder == "generated" && continue
+        @show folder_path = joinpath(main_path, folder)
+        try format(folder_path;
+            whitespace_ops_in_indices = true,
+            remove_extra_newlines = true
+            )
+        catch
+            @warn("Formatter Failed at file $(folder_path)")
+        end
+    end
+end
