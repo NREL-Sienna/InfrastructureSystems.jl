0e695f69b2b1f44eea519f5f7fdab647a9792b62
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 13:12:25 -0800

    change github action for error instead of PR

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -5,7 +5,8 @@ for main_path in main_paths
         @show folder_path = joinpath(main_path, folder)
         try format(folder_path;
             whitespace_ops_in_indices = true,
-            remove_extra_newlines = true
+            remove_extra_newlines = true,
+            verbose = true
             )
         catch
             @warn("Formatter Failed at file $(folder_path)")
