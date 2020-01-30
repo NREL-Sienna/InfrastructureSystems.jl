c34e603379c888616dcc32d38f10f0404b198b4a
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 13:28:17 -0800

    update formatter action

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -1,15 +1,15 @@
 main_paths = ["./src", "./test"]
 for main_path in main_paths
     for folder in readdir(main_path)
-        folder == "generated" && continue
+        occursin("generated", folder) && continue
         @show folder_path = joinpath(main_path, folder)
-        try format(folder_path;
+        if isfile(folder_path)
+            !occursin(".jl", folder_path) && continue
+        end
+        format(folder_path;
             whitespace_ops_in_indices = true,
             remove_extra_newlines = true,
             verbose = true
             )
-        catch
-            @warn("Formatter Failed at file $(folder_path)")
-        end
     end
 end
