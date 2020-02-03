c048df41edb345c725e1a5eafec1a89ac0617d92
Jose Daniel Lara <jdlara@berkeley.edu>
2020-02-03 09:21:11 -0800

    change formatter code options

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -9,7 +9,9 @@ for main_path in main_paths
         format(folder_path;
             whitespace_ops_in_indices = true,
             remove_extra_newlines = true,
-            verbose = true
+            verbose = true,
+            always_for_in = true,
+            whitespace_typedefs = true
             )
     end
 end
