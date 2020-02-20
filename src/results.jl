3e2870e002e3e35c599ecfc94d075fd9bcfe1206
Lily Hanig <lanig12345@gmail.com>
2020-02-20 13:47:23 -0700

    modify formatting

--- src/results.jl
+++ src/results.jl
@@ -1,21 +1,21 @@
 """To implement a sub-type of this you need to implement the methods below."""
 abstract type Results end
-function get_variables(r::T) where T <: Results
+function get_variables(r::T) where {T <: Results}
     error("get_variables must be implemented for $T")
 end
 
-function get_total_cost(r::T) where T <: Results
+function get_total_cost(r::T) where {T <: Results}
     error("get_total_cost must be implemented for $T")
 end
 
-function get_optimizer_log(r::T) where T <: Results
+function get_optimizer_log(r::T) where {T <: Results}
     error("get_optimizer_log must be implemented for $T")
 end
 
-function get_time_stamp(r::T) where T <: Results
+function get_time_stamp(r::T) where {T <: Results}
     error("get_time_stamp must be implemented for $T")
 end
 
-function write_results(r::T) where T <: Results
+function write_results(r::T) where {T <: Results}
     error("write_results must be implemented for $T")
 end
