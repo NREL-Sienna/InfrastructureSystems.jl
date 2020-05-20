4607fd528f387a3cbb596be10dbf411d0e3bf23e
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-05-20 17:16:15 -0600

    adding constructors for CostCoefficient

--- src/supplemental_constructors.jl
+++ src/supplemental_constructors.jl
@@ -160,6 +160,23 @@ function ScenarioBased(
     return ScenarioBased(label, data)
 end
 
+function ScenarioBased(forecasts::Vector{ScenarioBased})
+    @assert !isempty(forecasts)
+    scenario_count = get_scenario_count(forecasts[1])
+    colnames = TimeSeries.colnames(get_data(forecasts[1]))
+    data = Array{Any}(undef, 0, scenario_count)
+    timestamps =
+        collect(Iterators.flatten((TimeSeries.timestamp(get_data(x)) for x in forecasts)))
+    for x in forecasts
+        data = vcat(data, TimeSeries.values(get_data(x)))
+    end
+    ta = TimeSeries.TimeArray(timestamps, data, colnames)
+
+    forecast = ScenarioBased(get_label(forecasts[1]), ta)
+    @debug "concatenated forecasts" forecast
+    return forecast
+end
+
 function make_public_forecast(forecast::ScenarioBasedInternal, data::TimeSeries.TimeArray)
     return ScenarioBased(get_label(forecast), data)
 end
@@ -175,6 +192,67 @@ function make_internal_forecast(forecast::ScenarioBased, ts_data::TimeSeriesData
     )
 end
 
+function CostCoefficient(label::String, data::TimeSeries.TimeArray)
+    initial_time = TimeSeries.timestamp(data)[1]
+    resolution = get_resolution(data)
+    breakpoints = length(TimeSeries.colnames(data))/2
+    return CostCoefficient(label, breakpoints, data)
+end
+
+"""
+Constructs CostCoefficient Forecast after constructing a TimeArray from initial_time and
+time_steps.
+"""
+function CostCoefficient(
+    label::String,
+    resolution::Dates.Period,
+    initial_time::Dates.DateTime,
+    break_points::Int,
+    time_steps::Int,
+)
+    name = collect(Iterators.flatten([
+        (Symbol("cost_bp$(ix)"), Symbol("load_bp$ix")) for ix in 1:break_points]))
+    data = TimeSeries.TimeArray(
+        initial_time:resolution:(initial_time + resolution * (time_steps - 1)),
+        ones(time_steps, break_points),
+        name,
+    )
+
+    return CostCoefficient(label, break_points, data)
+end
+
+function CostCoefficient(forecasts::Vector{CostCoefficient})
+    @assert !isempty(forecasts)
+    break_points = get_break_points(forecasts[1])
+    colnames = TimeSeries.colnames(get_data(forecasts[1]))
+    data = Array{Any}(undef, 0, break_points)
+    timestamps =
+        collect(Iterators.flatten((TimeSeries.timestamp(get_data(x)) for x in forecasts)))
+    for x in forecasts
+        data = vcat(data, TimeSeries.values(get_data(x)))
+    end
+    ta = TimeSeries.TimeArray(timestamps, data, colnames)
+
+    forecast = CostCoefficient(get_label(forecasts[1]), break_points, ta)
+    @debug "concatenated forecasts" forecast
+    return forecast
+end
+
+function make_public_forecast(forecast::CostCoefficientInternal, data::TimeSeries.TimeArray)
+    return CostCoefficient(get_label(forecast), get_break_points(forecast), data)
+end
+
+function make_internal_forecast(forecast::CostCoefficient, ts_data::TimeSeriesData)
+    return CostCoefficientInternal(
+        get_label(forecast),
+        get_resolution(forecast),
+        get_initial_time(forecast),
+        get_break_points(forecast),
+        get_uuid(ts_data),
+        get_horizon(forecast),
+    )
+end
+
 function forecast_external_to_internal(::Type{T}) where {T <: Forecast}
     if T <: Deterministic
         forecast_type = DeterministicInternal
@@ -182,6 +260,8 @@ function forecast_external_to_internal(::Type{T}) where {T <: Forecast}
         forecast_type = ProbabilisticInternal
     elseif T <: ScenarioBased
         forecast_type = ScenarioBasedInternal
+    elseif T <: CostCoefficient
+        forecast_type = CostCoefficientInternal
     else
         @assert false
     end
@@ -196,6 +276,8 @@ function forecast_internal_to_external(::Type{T}) where {T <: ForecastInternal}
         forecast_type = Probabilistic
     elseif T <: ScenarioBasedInternal
         forecast_type = ScenarioBased
+    elseif T <: CostCoefficientInternal 
+        forecast_type = CostCoefficient
     else
         @assert false
     end
