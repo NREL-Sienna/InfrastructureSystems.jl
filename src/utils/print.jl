cce15a0f6ea4f07ad38ceb67f9af050bfcf803b2
Raphael Saavedra <raphael.saavedra93@gmail.com>
2020-08-19 15:18:02 +0100

    Fix printing of undefined components

--- src/utils/print.jl
+++ src/utils/print.jl
@@ -131,8 +131,8 @@ end
 
 function Base.show(io::IO, ::MIME"text/plain", ists::Vector{<:InfrastructureSystemsType})
     println(io, summary(ists))
-    for ist in ists
-        println(io, "$(summary(ist))")
+    for i = 1:length(ists)
+        isassigned(ists, i) && println(io, "$(summary(ists[i]))")
     end
 end
 
