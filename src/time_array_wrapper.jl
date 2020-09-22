6f3c083cdf80d6296d1be0288c2dce63879409ce
Jose Daniel Lara <jdlara@berkeley.edu>
2020-09-21 23:16:31 -0700

    reimplement time array wrapper

--- src/time_array_wrapper.jl
+++ src/time_array_wrapper.jl
@@ -1,18 +1,28 @@
 struct TimeArrayWrapper <: InfrastructureSystemsType
-    data::TimeSeries.TimeArray
+    data::DataStructures.SortedDict{Dates.DateTime, TimeSeries.TimeArray}
     internal::InfrastructureSystemsInternal
 
     function TimeArrayWrapper(data, internal)
-        if length(data) < 2
-            throw(ArgumentError("time array length must be at least 2"))
+        for v in values(data)
+            if length(v) < 2
+                throw(ArgumentError("time array length must be at least 2"))
+            end
         end
-
         return new(data, internal)
     end
 end
 
-function TimeArrayWrapper(data)
+function TimeArrayWrapper(data::TimeSeries.TimeArray)
+    data_= Dict(first(TimeSeries.timestamp(data)) => data)
+    return TimeArrayWrapper(data_, InfrastructureSystemsInternal())
+end
+
+function TimeArrayWrapper(data::DataStructures.SortedDict{Dates.DateTime, TimeSeries.TimeArray})
     return TimeArrayWrapper(data, InfrastructureSystemsInternal())
+end
+
+function TimeArrayWrapper(data::Dict{Dates.DateTime, TimeSeries.TimeArray})
+    return TimeArrayWrapper(DataStructures.SortedDict(data...), InfrastructureSystemsInternal())
 end
 
 get_internal(data) = data.internal
@@ -26,8 +36,9 @@ function Base.show(io::IO, ::MIME"text/plain", ta::TimeArrayWrapper)
     println(io, "data=$(ta.data)")
 end
 
-Base.length(ta::TimeArrayWrapper) = length(ta.data)
-get_initial_time(ta::TimeArrayWrapper) = TimeSeries.timestamp(ta.data)[1]
-get_horizon(ta::TimeArrayWrapper) = length(ta.data)
+Base.length(ta::TimeArrayWrapper) = length(first(values(ta.data)))
+# TODO: Not super efficient for now.
+get_initial_time(ta::TimeArrayWrapper) = sort(collect(keys(ta.data)))[1]
+get_horizon(ta::TimeArrayWrapper) = length(first(values(ta.data)))
 get_resolution(ta::TimeArrayWrapper) =
-    TimeSeries.timestamp(ta.data)[2] - TimeSeries.timestamp(ta.data)[1]
+    TimeSeries.timestamp(first(values(ta.data)))[2] - TimeSeries.timestamp(first(values(ta.data)))[1]
