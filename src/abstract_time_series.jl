e6191e6b2b62ec1f647e39c8c16707ae678b77eb
Daniel Thom <daniel.thom@nrel.gov>
2020-09-24 17:00:42 -0600

    Fix docstrings

--- src/abstract_time_series.jl
+++ src/abstract_time_series.jl
@@ -1,7 +1,7 @@
 """
 Abstract type for time_series that are stored in a system.
 Users never create them or get access to them.
-Stores references to time series data, so a disk read may be required for access.
+Stores references to time series data.
 """
 abstract type TimeSeriesMetadata <: InfrastructureSystemsType end
 
@@ -9,11 +9,21 @@ abstract type ForecastMetadata <: TimeSeriesMetadata end
 
 abstract type StaticTimeSeriesMetadata <: TimeSeriesMetadata end
 
+function get_time_series_initial_times(ts_metadata::TimeSeriesMetadata)
+    initial_time_stamp = get_initial_time_stamp(ts_metadata)
+    interval = get_interval(ts_metadata)
+    count = get_count(ts_metadata)
+    # TODO DT: is collect required?
+    return collect(range(initial_time_stamp; length = count, step = interval))
+end
+
+get_initial_time_stamp(ts::StaticTimeSeriesMetadata) = get_initial_time(ts)
+Base.length(ts::StaticTimeSeriesMetadata) = get_length(ts)
+
 """
 Abstract type for time_series supplied to users. They are not stored in a system. Instead,
 they are generated on demand for the user.
 Users can create them. The system will convert them to a subtype of TimeSeriesMetadata for
 storage.
-Time series data is stored as a field, so reads will always be from memory.
 """
 abstract type TimeSeriesData end
