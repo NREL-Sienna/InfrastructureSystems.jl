d0713f36d55089cf0fe7210aefb55a1e9ec84be3
Jose Daniel Lara <jdlara@berkeley.edu>
2020-09-28 09:28:33 -0700

    remove references to get_timestep

--- src/time_series_formats.jl
+++ src/time_series_formats.jl
@@ -199,15 +199,10 @@ function read_time_series(
 ) where {
     T <: Union{TimeSeriesFormatPeriodAsColumn, TimeSeriesFormatDateTimeAsColumn},
 }
-    # All timestamps must be sequential by step, so we can ignore the timestamps in the
-    # file after the first one.
-    # They were validated in get_step_time.
     first_timestamp = get_timestamp(T, file, 1)
-
     value_columns = get_value_columns(T, file)
     vals = [(string(x) => getproperty(file, x)) for x in value_columns]
     series_length = length(vals[1][2])
-
     return RawTimeSeries(first_timestamp, Dict(vals...), series_length)
 end
 
@@ -224,11 +219,6 @@ function read_time_series(
 ) where {T <: TimeSeriesFormatPeriodAsHeader}
     period_cols_as_symbols = get_period_columns(T, file)
     period = [parse(Int, string(x)) for x in period_cols_as_symbols]
-
-    # All timestamps must be sequential by step, so we can ignore the timestamps in the
-    # file after the first one.
-    # They were validated in get_step_time.
-
     first_time_stamp = get_timestamp(T, file, 1)
     vals = Vector{Float64}()
     for i in 1:length(file)
@@ -294,53 +284,4 @@ function get_num_steps(
     period::AbstractArray,
 ) where {T <: TimeSeriesFormatPeriodAsHeader}
     return num_steps = period[end]
-end
-
-"""
-Return a DateTime for the step between values as specified by the period in the file.
-"""
-function get_step_time(
-    ::Type{T},
-    file::CSV.File,
-    period::AbstractArray,
-) where {T <: TimeSeriesFileFormat}
-    timestamps = get_unique_timestamps(T, file)
-    num_steps = get_num_steps(T, file, period)
-
-    @debug timestamps, num_steps
-    if length(timestamps) == 1
-        # TODO: Not sure how to handle this. We could make specific functions for each type.
-        # For any YMD format the lowest resolution is Day.
-        # What is it for DateTime? We can't infer from one value.
-        resolution = Dates.Day(1)
-    else
-        resolution = timestamps[2]["timestamp"] - timestamps[1]["timestamp"]
-        if length(timestamps) > 2
-            for i in 3:length(timestamps)
-                diff = timestamps[i]["timestamp"] - timestamps[i - 1]["timestamp"]
-                if diff != resolution
-                    msg = "conflicting resolution=$resolution i=$i diff=$diff"
-                    throw(DataFormatError(msg))
-                end
-            end
-        end
-    end
-
-    return calculate_step_time(resolution, num_steps)
-end
-
-function get_step_time(
-    ::Type{T},
-    file::CSV.File,
-) where {T <: TimeSeriesFormatComponentsAsColumnsNoTime}
-    resolution = Dates.Day(1)
-    num_steps = length(file)
-    return calculate_step_time(resolution, num_steps)
-end
-
-function calculate_step_time(resolution::Dates.Period, num_steps::Int)
-    # Seconds should be the lowest possible resolution.
-    step = Dates.Second(resolution) / num_steps
-    @debug "file has step time of $step" resolution num_steps
-    return step
 end
