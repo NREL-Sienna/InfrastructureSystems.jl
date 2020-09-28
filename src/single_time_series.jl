9a6927ffbcaac53ec09784280a04065ab0a68942
Jose Daniel Lara <jdlara@berkeley.edu>
2020-09-28 14:37:07 -0700

    add missing element in the docstring

--- src/single_time_series.jl
+++ src/single_time_series.jl
@@ -38,6 +38,7 @@ component.
 # Arguments
 - `name::AbstractString`: user-defined name
 - `filename::AbstractString`: name of CSV file containing data
+- `component::InfrastructureSystemsComponent`: component associated with the data
 - `normalization_factor::NormalizationFactor = 1.0`: optional normalization factor to apply
   to each data entry
 - `scaling_factor_multiplier::Union{Nothing, Function} = nothing`: If the data are scaling
