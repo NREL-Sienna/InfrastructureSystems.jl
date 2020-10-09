3d27a25699b781f95c3b1f47b176fb8f44e10cd2
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-10-09 09:17:40 -0600

    removing alias from internal

--- src/internal.jl
+++ src/internal.jl
@@ -10,9 +10,6 @@ abstract type UnitsData end
 end
 
 const _UNIT_SYSTEM_MAP = Dict(string(x) => x for x in instances(UnitSystem))
-const CONSTANT = Real
-const POLYNOMIAL = Tuple{Float64, Float64}
-const PWL = Vector{Tuple{Float64, Float64}}
 
 mutable struct SystemUnitsSettings <: UnitsData
     base_value::Float64
