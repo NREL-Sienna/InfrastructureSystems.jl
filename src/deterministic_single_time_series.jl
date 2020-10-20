dcee549da501d861d82d9712866ac255ab6b71f1
Daniel Thom <daniel.thom@nrel.gov>
2020-10-20 12:03:24 -0600

    Add missing interface function

--- src/deterministic_single_time_series.jl
+++ src/deterministic_single_time_series.jl
@@ -1,4 +1,6 @@
 eltype_data(ts::DeterministicSingleTimeSeries) = eltype_data(ts.single_time_series)
+get_scaling_factor_multiplier(ts::DeterministicSingleTimeSeries) =
+    get_scaling_factor_multiplier(ts.single_time_series)
 
 function get_array_for_hdf(forecast::DeterministicSingleTimeSeries)
     return get_array_for_hdf(forecast.single_time_series)
