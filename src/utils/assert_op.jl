1635b68985e61fbdd100daabecc9263c7deea53a
Daniel Thom <daniel.thom@nrel.gov>
2020-11-10 16:55:38 -0700

    Add support for single expression

--- src/utils/assert_op.jl
+++ src/utils/assert_op.jl
@@ -1,31 +1,46 @@
 """
 Throw an `AssertionError` if `op(exp1, exp2)` is `false`.
 
-Use only the forms below. `@assert_op a == b` is not currently supported.
-
 # Examples
 ```
 julia> a = 3; b = 4;
-julia> @assert_op(a, ==, b)
-ERROR: AssertionError: a = 3, b = 4
+julia> @assert_op a == b
+ERROR: AssertionError: 3 is not isequal 4
 
-julia> IS.@assert_op(a + 3, ==, b + 4)
-ERROR: AssertionError: a + 3 = 6, b + 4 = 8
+julia> @assert_op a + 3 == b + 4
+ERROR: AssertionError: 6 is not == 8
 
 julia> IS.@assert_op a isequal b
-ERROR: AssertionError: a = 3, b = 4
+ERROR: AssertionError: 3 is not isequal 4
 ```
 """
 macro assert_op(exp1, op, exp2)
+    assert_op(exp1, op, exp2)
+end
+
+macro assert_op(expr...)
+    assert_op(expr)
+end
+
+function assert_op(expr::Expr)
+    @assert expr.head == :call
+    @assert length(expr.args) == 3
+
+    assert_op(expr.args[2], expr.args[1], expr.args[3])
+end
+
+function assert_op(expr::Tuple{Expr})
+    @assert length(expr) == 1
+    assert_op(expr[1])
+end
+
+function assert_op(exp1, op, exp2)
     return :(
-        if $op($(esc(exp1)), $(esc(exp2)))
-            return nothing
-        else
-            name1 = $(string(exp1))
-            name2 = $(string(exp2))
+        if !$op($(esc(exp1)), $(esc(exp2)))
             val1 = $(esc(exp1))
             val2 = $(esc(exp2))
-            throw(AssertionError("$name1 = $val1, $name2 = $val2"))
+            op_str = $(esc(op))
+            throw(AssertionError("$val1 is not $op_str $val2"))
         end
     )
 end
--- test/test_assert_op.jl
+++ test/test_assert_op.jl
@@ -1,10 +1,11 @@
 @testset "Test assert_op" begin
     a = 2
     b = 2
+    IS.@assert_op a == b
     IS.@assert_op(a, ==, b)
     IS.@assert_op(a, isequal, b)
     IS.@assert_op a isequal b
-    IS.@assert_op(a + 2, ==, b + 2)
+    IS.@assert_op a + 2 == b + 2
     @test_throws AssertionError IS.@assert_op a + 2 isequal b + 3
-    @test_throws AssertionError IS.@assert_op(a + 2, ==, b + 3)
+    @test_throws AssertionError IS.@assert_op a + 2 == b + 3
 end
