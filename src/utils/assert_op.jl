7973a24d82f938999fa3d93d5a8173140953ffb8
Dheepak Krishnamurthy <me@kdheepak.com>
2020-11-12 15:46:08 -0700

    Refactor `@assert_op`

--- src/utils/assert_op.jl
+++ src/utils/assert_op.jl
@@ -1,37 +1,39 @@
 """
-Throw an `AssertionError` if `op(exp1, exp2)` is `false`.
+Throw an `AssertionError` if conditions like `op(exp1, exp2)` are `false`, where `op` is a conditional infix operator.
 
 # Examples
+
 ```
 julia> a = 3; b = 4;
 julia> @assert_op a == b
-ERROR: AssertionError: 3 is not isequal 4
+ERROR: AssertionError: 3 == 4
 
-julia> @assert_op a + 3 == b + 4
-ERROR: AssertionError: 6 is not == 8
-
-julia> IS.@assert_op a isequal b
-ERROR: AssertionError: 3 is not isequal 4
+julia> @assert_op a + 3 > b + 4
+ERROR: AssertionError: 6 > 8
 ```
 """
-macro assert_op(exp1, op, exp2)
-    assert_op(exp1, op, exp2)
-end
-
-macro assert_op(expr...)
+macro assert_op(expr)
     assert_op(expr)
 end
 
 function assert_op(expr::Expr)
-    @assert expr.head == :call
-    @assert length(expr.args) == 3
-
-    assert_op(expr.args[2], expr.args[1], expr.args[3])
+    # Only special case expressions of the form `expr1 == expr2`
+    if length(expr.args) == 3 && expr.head == :call
+        return assert_op(expr.args[1], expr.args[2], expr.args[3])
+    else
+        return :(@assert $(expr))
+    end
 end
 
-function assert_op(expr::Tuple{Expr})
-    @assert length(expr) == 1
-    assert_op(expr[1])
+function assert_op(op, exp1, exp2)
+    return :(
+        if !$op($(esc(exp1)), $(esc(exp2)))
+            val1 = $(esc(exp1))
+            val2 = $(esc(exp2))
+            op_str = $(esc(op))
+            throw(AssertionError("$val1 $op_str $val2"))
+        end
+    )
 end
 
 function assert_op(exp1, op, exp2)
--- test/test_assert_op.jl
+++ test/test_assert_op.jl
@@ -2,10 +2,9 @@
     a = 2
     b = 2
     IS.@assert_op a == b
-    IS.@assert_op(a, ==, b)
-    IS.@assert_op(a, isequal, b)
-    IS.@assert_op a isequal b
     IS.@assert_op a + 2 == b + 2
-    @test_throws AssertionError IS.@assert_op a + 2 isequal b + 3
-    @test_throws AssertionError IS.@assert_op a + 2 == b + 3
+    IS.@assert_op isequal(a + 2, b + 2)
+
+    @test_throws AssertionError IS.@assert_op a + 3 == b + 2
+    @test_throws AssertionError IS.@assert_op isequal(a + 3, b + 2)
 end
