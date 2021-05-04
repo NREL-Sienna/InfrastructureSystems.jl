4c0cbd41e9d0e044461237e6e1995ba56462eff4
Jose Daniel Lara <jdlara@berkeley.edu>
2021-05-04 08:22:42 -0700

    Update src/deprecated.jl
    Co-authored-by: Nick Robinson <nicholas.robinson@invenialabs.co.uk>

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -4,7 +4,7 @@ function _add_component_kwarg_deprecation(kwargs)
     if haskey(kwargs, :deserialization_in_progress)
         Base.depwarn("Keyword deserialization_in_progress is deprecated, use allow_existing_time_series instead.", :add_component!)
         kw = Dict(k => v for (k, v) in kwargs if k != :deserialization_in_progress)
-        kw[:allow_existing_time_series] = get(kwargs, :deserialization_in_progress, true)
+        kw[:allow_existing_time_series] = kwargs[:deserialization_in_progress]
         return kw
     else
         return kwargs
