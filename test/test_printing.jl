3222f2ba209c0ec2cde1612c5ebbb2ae6d382b6c
Daniel Thom <daniel.thom@nrel.gov>
2021-08-30 17:56:59 -0600

    Add unit tests

---
+++ test/test_printing.jl
@@ -0,0 +1,25 @@
+@testset "Test printing of the system and components" begin
+    sys = create_system_data(with_time_series = true, time_series_in_memory = true)
+    io = IOBuffer()
+    show(io, "text/plain", sys)
+    text = String(take!(io))
+    @test occursin("TestComponent", text)
+    @test occursin("Time Series Summary", text)
+end
+
+@testset "Test show_component_tables" begin
+    sys = create_system_data(with_time_series = true, time_series_in_memory = true)
+    io = IOBuffer()
+    IS.show_components(io, sys.components, IS.TestComponent)
+    @test occursin("TestComponent", String(take!(io)))
+
+    IS.show_components(io, sys.components, IS.TestComponent, [:val])
+    text = String(take!(io))
+    @test occursin("TestComponent", text)
+    @test occursin("val", text)
+
+    IS.show_components(io, sys.components, IS.TestComponent, Dict("val" => x -> x.val * 10))
+    text = String(take!(io))
+    @test occursin("TestComponent", text)
+    @test occursin("val", text)
+end
