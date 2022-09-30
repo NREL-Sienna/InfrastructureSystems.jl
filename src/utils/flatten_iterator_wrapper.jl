c00f186e08c5268419910d05754db118bc8fab31
Jose Daniel Lara <jdlara@berkeley.edu>
2022-09-29 18:03:10 -0700

    fix formatter

--- src/utils/flatten_iterator_wrapper.jl
+++ src/utils/flatten_iterator_wrapper.jl
@@ -2,22 +2,22 @@
 """
 Wrapper around Iterators.Flatten to provide total length.
 """
-struct FlattenIteratorWrapper{T,I}
+struct FlattenIteratorWrapper{T, I}
     iter::Iterators.Flatten{I}
     length::Int
 end
 
-function FlattenIteratorWrapper(::Type{T}, vals::I) where {T,I}
+function FlattenIteratorWrapper(::Type{T}, vals::I) where {T, I}
     len = isempty(vals) ? 0 : sum((length(x) for x in vals))
-    return FlattenIteratorWrapper{T,I}(Iterators.Flatten(vals), len)
+    return FlattenIteratorWrapper{T, I}(Iterators.Flatten(vals), len)
 end
 
 Base.@propagate_inbounds function Base.iterate(
-    iter::FlattenIteratorWrapper{T,I},
-    state=()
-) where {T,I}
+    iter::FlattenIteratorWrapper{T, I},
+    state=(),
+) where {T, I}
     Base.iterate(iter.iter, state)
 end
 
-Base.eltype(::FlattenIteratorWrapper{T,I}) where {T,I} = T
+Base.eltype(::FlattenIteratorWrapper{T, I}) where {T, I} = T
 Base.length(iter::FlattenIteratorWrapper) = iter.length
