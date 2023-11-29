fda144e94e22ef848da34d595bf0de9d662dd9db
Jose Daniel Lara <jdlara@berkeley.edu>
2023-11-29 10:18:13 -0500

    add some additional functionality for geo info

--- src/geographic_info.jl
+++ src/geographic_info.jl
@@ -4,14 +4,16 @@ Base type for structs that store infos
 Required interface functions for subtypes:
 
   - get_internal()
-
-Optional interface functions:
-
-  - get_time_series_container()
+  - get_components_uuid()
 
 Subtypes may contain time series.
 """
 struct InfrastructureSystemsGeo <: InfrastructureSystemsInfo
     geo_json::Dict{String, Any}
+    components_uuid::Vector{UUIDs.UUID}
     internal::InfrastructureSystemsInternal
 end
+
+get_internal(::InfrastructureSystemsGeo) = geo.internal
+get_uuid(geo::InfrastructureSystemsGeo) = get_uuid(get_internal(geo))
+get_geo_json(geo::InfrastructureSystemsGeo) = geo.geo_json
