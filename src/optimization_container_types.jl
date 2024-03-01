8c9898af5b4d59ebfecc7fbbe06b2ecf60b1809b
Jose Daniel Lara <jdlara@berkeley.edu>
2024-02-29 17:26:54 -0700

    add optimization container metadata

--- src/optimization_container_types.jl
+++ src/optimization_container_types.jl
@@ -1,4 +1,4 @@
-abstract type AbstractModelContainer end
+abstract type AbstractOptimizationContainer end
 
 abstract type VariableType end
 abstract type ConstraintType end
@@ -6,3 +6,11 @@ abstract type AuxVariableType end
 abstract type ParameterType end
 abstract type InitialConditionType end
 abstract type ExpressionType end
+
+"""
+Optimization Container construction stage
+"""
+abstract type ConstructStage end
+
+struct ArgumentConstructStage <: ConstructStage end
+struct ModelConstructStage <: ConstructStage end
