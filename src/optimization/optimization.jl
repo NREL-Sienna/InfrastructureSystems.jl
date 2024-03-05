e71d9dda97c25c5e3bdb506055f7b8505340f4d1
Jose Daniel Lara <jdlara@berkeley.edu>
2024-03-04 17:22:42 -0700

    move files to folder for submodule

---
+++ src/optimization/optimization.jl
@@ -0,0 +1,16 @@
+"""
+    Optimization
+"""
+module Optimization
+
+include("abstract_model_store.jl")
+include("abstract_model_store_params.jl")
+include("model_internal.jl")
+include("optimization_container_keys.jl")
+include("optimization_container_metadata.jl")
+include("optimization_container_types.jl")
+include("optimizer_stats.jl")
+include("problem_results.jl")
+include("problem_results_export.jl")
+
+end
