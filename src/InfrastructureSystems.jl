00e2923eba57acefc74a0dfdc8e99d04e1ffab15
Jose Daniel Lara <jdlara@berkeley.edu>
2024-03-04 20:00:12 -0700

    use renamed file

--- src/InfrastructureSystems.jl
+++ src/InfrastructureSystems.jl
@@ -149,6 +149,6 @@ include("utils/print.jl")
 include("utils/test.jl")
 include("units.jl")
 include("deprecated.jl")
-include("Optimization/Optimization2.jl")
+include("Optimization/Optimization.jl")
 
 end # module
