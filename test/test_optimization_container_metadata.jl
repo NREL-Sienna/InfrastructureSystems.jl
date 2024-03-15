51cb4c039b37620aed41117bf8a531fb13245545
Jose Daniel Lara <jdlara@berkeley.edu>
2024-03-15 17:22:03 -0600

    add test for optimization container metada

---
+++ test/test_optimization_container_metadata.jl
@@ -0,0 +1,26 @@
+import InfrastructureSystems.Optimization:
+    VariableKey,
+    OptimizationContainerMetadata,
+    encode_key_as_string
+
+@testset "Testset Optimization Container Metadata" begin
+    metadata = OptimizationContainerMetadata()
+    var_key = VariableKey(IS.Optimization.MockVariable, IS.TestComponent)
+    IS.Optimization.add_container_key!(metadata, encode_key_as_string(var_key), var_key)
+    @test IS.Optimization.has_container_key(metadata, encode_key_as_string(var_key))
+    @test IS.Optimization.get_container_key(metadata, encode_key_as_string(var_key)) ==
+          var_key
+    file_dir = mktempdir()
+    model_name = :MockModel
+    IS.Optimization.serialize_metadata(file_dir, metadata, model_name)
+    file_path = IS.Optimization._make_metadata_filename(file_dir, model_name)
+    deserialized_metadata = IS.Optimization.deserialize_metadata(
+        OptimizationContainerMetadata,
+        file_dir,
+        model_name,
+    )
+    @test IS.Optimization.has_container_key(
+        deserialized_metadata,
+        encode_key_as_string(var_key),
+    )
+end
