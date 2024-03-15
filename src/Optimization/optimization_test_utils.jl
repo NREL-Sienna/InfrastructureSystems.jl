e92db391a82eca69220591fc86c5884638799221
Jose Daniel Lara <jdlara@berkeley.edu>
2024-03-15 16:35:05 -0600

    add keys tests

--- src/Optimization/optimization_test_utils.jl
+++ src/Optimization/optimization_test_utils.jl
@@ -1,1 +1,8 @@
 struct MockContainer <: AbstractOptimizationContainer end
+
+struct MockVariable <: VariableType end
+struct MockConstraint <: ConstraintType end
+struct MockAuxVariable <: AuxVariableType end
+struct MockExpresssion <: ExpressionType end
+struct MockParameter <: ParameterType end
+struct MockInitialCondition <: InitialConditionType end
