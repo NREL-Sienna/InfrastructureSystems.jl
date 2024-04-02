31f80059adc9359191ed384b95f7f8110210f6e4
Jose Daniel Lara <jdlara@berkeley.edu>
2024-04-02 10:15:53 -0600

    WIP store refactoring

--- src/Optimization/abstract_model_store.jl
+++ src/Optimization/abstract_model_store.jl
@@ -1,7 +1,36 @@
+# Store const definitions
+# Update src/simulation/simulation_store_common.jl with any changes.
+const STORE_CONTAINER_DUALS = :duals
+const STORE_CONTAINER_PARAMETERS = :parameters
+const STORE_CONTAINER_VARIABLES = :variables
+const STORE_CONTAINER_AUX_VARIABLES = :aux_variables
+const STORE_CONTAINER_EXPRESSIONS = :expressions
+const STORE_CONTAINERS = (
+    STORE_CONTAINER_DUALS,
+    STORE_CONTAINER_PARAMETERS,
+    STORE_CONTAINER_VARIABLES,
+    STORE_CONTAINER_AUX_VARIABLES,
+    STORE_CONTAINER_EXPRESSIONS,
+)
+
+# Keep these in sync with the Symbols in src/core/definitions.
+get_store_container_type(::AuxVarKey) = STORE_CONTAINER_AUX_VARIABLES
+get_store_container_type(::ConstraintKey) = STORE_CONTAINER_DUALS
+get_store_container_type(::ExpressionKey) = STORE_CONTAINER_EXPRESSIONS
+get_store_container_type(::ParameterKey) = STORE_CONTAINER_PARAMETERS
+get_store_container_type(::VariableKey) = STORE_CONTAINER_VARIABLES
+
 abstract type AbstractModelStore end
 
+# Required fields for subtypes
+# - :duals
+# - :parameters
+# - :variables
+# - :aux_variables
+# - :expressions
+
 # Required methods for subtypes:
-# = initialize_storage!
+# - initialize_storage!
 # - write_result!
 # - read_results
 # - write_optimizer_stats!
@@ -9,29 +38,27 @@ abstract type AbstractModelStore end
 #
 # Each subtype must have a field for each instance of STORE_CONTAINERS.
 
-function Base.empty!(store::AbstractModelStore)
-    stype = typeof(store)
-    for (name, type) in zip(fieldnames(stype), fieldtypes(stype))
+function Base.empty!(store::T) where {T <: AbstractModelStore}
+    for (name, type) in zip(fieldnames(T), fieldtypes(T))
         val = get_data_field(store, name)
         try
             empty!(val)
         catch
-            @error "Base.empty! must be customized for type $stype or skipped"
+            @error "Base.empty! must be customized for type $T or skipped"
             rethrow()
         end
     end
 end
 
-get_data_field(store::AbstractModelStore, type) = getproperty(store, type)
+get_data_field(store::AbstractModelStore, type::Symbol) = getproperty(store, type)
 
-function Base.isempty(store::AbstractModelStore)
-    stype = typeof(store)
-    for (name, type) in zip(fieldnames(stype), fieldtypes(stype))
+function Base.isempty(store::T) where {T <: AbstractModelStore}
+    for (name, type) in zip(fieldnames(T), fieldtypes(T))
         val = get_data_field(store, name)
         try
             !isempty(val) && return false
         catch
-            @error "Base.isempty must be customized for type $stype or skipped"
+            @error "Base.isempty must be customized for type $T or skipped"
             rethrow()
         end
     end
@@ -43,57 +70,47 @@ function list_fields(store::AbstractModelStore, container_type::Symbol)
     return keys(get_data_field(store, container_type))
 end
 
-function write_result!(store::AbstractModelStore, key, index, array)
-    field = get_store_container_type(key)
-    return write_result!(store, field, key, index, array)
-end
-
-function read_results(store::AbstractModelStore, key; index = nothing)
-    field = get_store_container_type(key)
-    return read_results(store, field, key; index = index)
-end
-
-function list_keys(store::AbstractModelStore, container_type)
+function list_keys(store::AbstractModelStore, container_type::Symbol)
     container = get_data_field(store, container_type)
     return collect(keys(container))
 end
 
-function get_variable_value(
+function get_value(
     store::AbstractModelStore,
     ::T,
     ::Type{U},
 ) where {T <: VariableType, U <: InfrastructureSystemsType}
-    return get_data_field(store, :variables)[VariableKey(T, U)]
+    return get_data_field(store, STORE_CONTAINER_VARIABLES)[VariableKey(T, U)]
 end
 
-function get_aux_variable_value(
+function get_value(
     store::AbstractModelStore,
     ::T,
     ::Type{U},
 ) where {T <: AuxVariableType, U <: InfrastructureSystemsType}
-    return get_data_field(store, :aux_variables)[AuxVarKey(T, U)]
+    return get_data_field(store, STORE_CONTAINER_AUX_VARIABLES)[AuxVarKey(T, U)]
 end
 
-function get_dual_value(
+function get_value(
     store::AbstractModelStore,
     ::T,
     ::Type{U},
 ) where {T <: ConstraintType, U <: InfrastructureSystemsType}
-    return get_data_field(store, :duals)[ConstraintKey(T, U)]
+    return get_data_field(store, STORE_CONTAINER_DUALS)[ConstraintKey(T, U)]
 end
 
-function get_parameter_value(
+function get_value(
     store::AbstractModelStore,
     ::T,
     ::Type{U},
 ) where {T <: ParameterType, U <: InfrastructureSystemsType}
-    return get_data_field(store, :parameters)[ParameterKey(T, U)]
+    return get_data_field(store, STORE_CONTAINER_PARAMETERS)[ParameterKey(T, U)]
 end
 
-function get_expression_value(
+function get_value(
     store::AbstractModelStore,
     ::T,
     ::Type{U},
 ) where {T <: ExpressionType, U <: InfrastructureSystemsType}
-    return get_data_field(store, :expressions)[ExpressionKey(T, U)]
+    return get_data_field(store, STORE_CONTAINER_EXPRESSIONS)[ExpressionKey(T, U)]
 end
--- src/Optimization/optimization_test_utils.jl
+++ src/Optimization/optimization_test_utils.jl
@@ -10,3 +10,67 @@ struct MockInitialCondition <: InitialConditionType end
 struct MockVariable2 <: VariableType end
 convert_result_to_natural_units(::Type{MockVariable2}) = true
 should_write_resulting_value(::Type{MockVariable2}) = false
+
+struct MockStoreParams <: AbstractModelStoreParams
+    size::Integer
+end
+
+struct MockModelStore <: AbstractModelStore
+    duals::Dict{ConstraintKey, Matrix{Float64}}
+    parameters::Dict{ParameterKey, Matrix{Float64}}
+    variables::Dict{VariableKey, Matrix{Float64}}
+    aux_variables::Dict{AuxVarKey, Matrix{Float64}}
+    expressions::Dict{ExpressionKey, Matrix{Float64}}
+end
+
+function MockModelStore()
+    return MockModelStore(
+        Dict{ConstraintKey, Matrix{Float64}}(),
+        Dict{ParameterKey, Matrix{Float64}}(),
+        Dict{VariableKey, Matrix{Float64}}(),
+        Dict{AuxVarKey, Matrix{Float64}}(),
+        Dict{ExpressionKey, Matrix{Float64}}(),
+    )
+end
+
+function initialize_storage!(
+    store::MockModelStore,
+    container::AbstractOptimizationContainer,
+    params::MockStoreParams,
+)
+end
+
+function write_result!(
+    store::MockModelStore,
+    name::Symbol,
+    key::OptimizationContainerKey,
+    index::Int,
+    update_timestamp::Dates.DateTime,
+    array::Vector{Float64},
+)
+end
+
+function read_results(
+    store::MockModelStore,
+    key::OptimizationContainerKey;
+    index::Int)
+end
+
+function write_optimizer_stats!(
+    store::MockModelStore,
+    stats::OptimizerStats,
+    index::Int,
+)
+    if index in keys(store.optimizer_stats)
+        @warn "Overwriting optimizer stats"
+    end
+    store.optimizer_stats[index] = stats
+    return
+end
+
+function read_optimizer_stats(store::MockModelStore)
+    stats = [IS.to_namedtuple(x) for x in values(store.optimizer_stats)]
+    df = DataFrames.DataFrame(stats)
+    DataFrames.insertcols!(df, 1, :DateTime => keys(store.optimizer_stats))
+    return df
+end
