1f470313cbc88b5c3ec764a02b9ef919fe55e4f2
rodrigomha <rodrigomha@gmail.com>
2024-04-18 13:01:35 -0700

    update write results

--- src/Optimization/optimization_container_types.jl
+++ src/Optimization/optimization_container_types.jl
@@ -16,8 +16,9 @@ convert_result_to_natural_units(::Type{<:ParameterType}) = false
 should_write_resulting_value(::Type{<:VariableType}) = true
 should_write_resulting_value(::Type{<:ConstraintType}) = true
 should_write_resulting_value(::Type{<:AuxVariableType}) = true
-should_write_resulting_value(::Type{<:ExpressionType}) = true
-should_write_resulting_value(::Type{<:ParameterType}) = true
+should_write_resulting_value(::Type{<:ExpressionType}) = false
+# TODO: Piecewise linear parameter are broken to write
+should_write_resulting_value(::Type{<:ParameterType}) = false
 
 abstract type RightHandSideParameter <: ParameterType end
 abstract type ObjectiveFunctionParameter <: ParameterType end
