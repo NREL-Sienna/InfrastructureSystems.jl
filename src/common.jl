51331c6b9bb4297d427e8544c00aa29dc8568974
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2024-04-19 18:55:46 -0600

    Implement `UnimplementedError`

--- src/common.jl
+++ src/common.jl
@@ -22,6 +22,21 @@ struct HashMismatchError <: Exception
     msg::AbstractString
 end
 
+"""
+Indicate that the feature at hand happens to not be implemented for the given data even
+though it could be. If it is a category mistake to imagine this feature defined on that
+data, use another exception, like `TypeError` or `ArgumentError`.
+"""
+struct UnimplementedError <: Exception
+    msg::AbstractString
+end
+
+UnimplementedError(feature, data) =
+    UnimplementedError("$feature not currently implemented for $data")
+
+Base.showerror(io::IO, e::UnimplementedError) =
+    println(io, "$UnimplementedError: $(e.msg)")
+
 const CONSTANT = Float64
 const LIM_TOL = 1e-6
 const XY_COORDS = @NamedTuple{x::Float64, y::Float64}
