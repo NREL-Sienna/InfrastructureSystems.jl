29b82537d404c0c8dcaf051958e8d9799a527f19
Daniel Thom <daniel.thom@nrel.gov>
2024-04-24 18:15:28 -0600

    Change the supertype of TimeSeriesData

--- src/abstract_time_series.jl
+++ src/abstract_time_series.jl
@@ -19,7 +19,7 @@ Abstract type for time series stored in the system.
 Components store references to these through TimeSeriesMetadata values so that data can
 reside on storage media instead of memory.
 """
-abstract type TimeSeriesData <: InfrastructureSystemsComponent end
+abstract type TimeSeriesData <: InfrastructureSystemsType end
 
 # Subtypes must implement
 # - Base.length
