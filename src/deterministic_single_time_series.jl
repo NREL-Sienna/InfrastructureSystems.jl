b663078e90523a5e83fc21fff2a8cb00da523145
kdayday <kdayday@users.noreply.github.com>
2024-07-25 11:51:52 -0600

    Add DeterministicSingleTimeSeries detail from Time Series Data

--- src/deterministic_single_time_series.jl
+++ src/deterministic_single_time_series.jl
@@ -7,11 +7,19 @@
         horizon::Int
     end
 
-A deterministic forecast for a particular data field in a Component that wraps a SingleTimeSeries.
+A deterministic forecast that wraps a [`SingleTimeSeries`](@ref)
+
+`DeterministicSingleTimeSeries` behaves exactly like a [`Deterministic`](@ref), but
+instead of storing windows at each initial time it provides a view into the existing
+`SingleTimeSeries` at incrementing offsets. This avoids large data duplications when 
+there are the overlapping windows between forecasts. 
+
+Can be used as a perfect forecast based on historical data when real forecast data
+is unavailable. 
 
 # Arguments
 
-  - `single_time_series::SingleTimeSeries`: wrapped SingleTimeSeries object
+  - `single_time_series::SingleTimeSeries`: wrapped `SingleTimeSeries` object
   - `initial_timestamp::Dates.DateTime`: time series availability time
   - `interval::Dates.Period`: time step between forecast windows
   - `count::Int`: number of forecast windows
