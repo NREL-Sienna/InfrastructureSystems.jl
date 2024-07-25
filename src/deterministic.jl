5fb45ae1511437955e8b2f4649e98a00a1f39f55
kdayday <kdayday@users.noreply.github.com>
2024-07-25 15:21:38 -0600

    Add example of deterministic from existing

--- src/deterministic.jl
+++ src/deterministic.jl
@@ -199,6 +199,30 @@ Construct Deterministic that shares the data from an existing instance.
 
 This is useful in cases where you want a component to use the same time series data for
 two different attributes.
+
+# Examples
+```julia
+resolution = Dates.Hour(1)
+data = Dict(
+    DateTime("2020-01-01T00:00:00") => ones(24),
+    DateTime("2020-01-01T01:00:00") => ones(24),
+)
+# Define a Deterministic for the first attribute
+forecast_max_active_power = Deterministic(
+    "max_active_power",
+    data,
+    resolution,
+    scaling_factor_multiplier = get_max_active_power,
+)
+add_time_series!(sys, generator, forecast_max_active_power)
+# Reuse time series for second attribute
+forecast_max_reactive_power = Deterministic(
+    forecast_max_active_power,
+    "max_reactive_power"
+    scaling_factor_multiplier = get_max_reactive_power,
+)
+add_time_series!(sys, generator, forecast_max_reactive_power)
+```
 """
 function Deterministic(
     src::Deterministic,
