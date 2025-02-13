0f90fac7229d8f4eaaa080bcb85761cbc4be2a38
rodrigomha <rodrigomha@gmail.com>
2025-02-13 10:57:18 -0800

    update test imports

--- test/test_optimization_results.jl
+++ test/test_optimization_results.jl
@@ -3,8 +3,11 @@ import InfrastructureSystems.Optimization:
     OptimizationProblemResults,
     VariableKey,
     ExpressionKey,
+    MockVariable,
+    MockExpression,
     read_variable,
-    read_expression
+    read_expression,
+    convert_result_to_natural_units
 import Dates:
     DateTime,
     Millisecond
@@ -23,11 +26,11 @@ const IS = InfrastructureSystems
     data = IS.SystemData()
     uuid = IS.make_uuid()
     aux_variable_values = Dict()
-    var_key = VariableKey(IS.Optimization.MockVariable, IS.TestComponent)
+    var_key = VariableKey(MockVariable, IS.TestComponent)
     variable_values = Dict(var_key => DataFrame(["test" => [1.0, 2.0]]))
     dual_values = Dict()
     parameter_values = Dict()
-    exp_key = ExpressionKey(IS.Optimization.MockExpression, IS.TestComponent)
+    exp_key = ExpressionKey(MockExpression, IS.TestComponent)
     # Expression only 1 time-step
     expression_values = Dict(exp_key => DataFrame(["test2" => 1.0]))
     optimizer_stats = DataFrames.DataFrame()
@@ -71,14 +74,14 @@ const IS = InfrastructureSystems
     @test size(var_res) == (2, 2)
     @test length(var_res[!, "DateTime"]) == 2
     @test [1.0, 2.0] == var_res[!, 2]
-    IS.Optimization.convert_result_to_natural_units(::Type{<:IS.Optimization.MockVariable}) = true
+    convert_result_to_natural_units(::Type{<:MockVariable}) = true
     var_res = IS.Optimization.read_variable(opt_res1, var_key)
     @test var_res[!, 2] == [10.0, 20.0]
     # Check that expression only has a single column
     exp_res = read_expression(opt_res2, exp_key)
     @test size(exp_res) == (1, 1)
     @test exp_res[!, 1] == 1.0
-    IS.Optimization.convert_result_to_natural_units(::Type{<:IS.Optimization.MockExpression}) = true
+    convert_result_to_natural_units(::Type{<:MockExpression}) = true
     exp_res = read_expression(opt_res2, exp_key)
     @test exp_res[!, 1] == 10.0
 end
