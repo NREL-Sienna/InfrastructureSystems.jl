663b9fbb44e3cab1285a4cd11b27b3599f83d844
rodrigomha <rodrigomha@gmail.com>
2025-02-13 10:54:07 -0800

    update test

--- test/test_optimization_results.jl
+++ test/test_optimization_results.jl
@@ -11,7 +11,7 @@ import Dates:
 const IS = InfrastructureSystems
 
 @testset "Test OptimizationProblemResults" begin
-    base_power = 1.0
+    base_power = 10.0
     # 2 hours timestamp range
     timestamp_range =
         StepRange(
@@ -24,17 +24,17 @@ const IS = InfrastructureSystems
     uuid = IS.make_uuid()
     aux_variable_values = Dict()
     var_key = VariableKey(IS.Optimization.MockVariable, IS.TestComponent)
-    variable_values = Dict(var_key => DataFrame(["test" => 1:2]))
+    variable_values = Dict(var_key => DataFrame(["test" => [1.0, 2.0]]))
     dual_values = Dict()
     parameter_values = Dict()
     exp_key = ExpressionKey(IS.Optimization.MockExpression, IS.TestComponent)
     # Expression only 1 time-step
-    expression_values = Dict(exp_key => DataFrame(["test2" => 1]))
+    expression_values = Dict(exp_key => DataFrame(["test2" => 1.0]))
     optimizer_stats = DataFrames.DataFrame()
     metadata = OptimizationContainerMetadata()
     # Test with StepRange
     opt_res1 = OptimizationProblemResults(
-        1.0,
+        base_power,
         timestamp_range,
         data,
         uuid,
@@ -51,7 +51,7 @@ const IS = InfrastructureSystems
     )
     # Test with Vector{DateTime}
     opt_res2 = OptimizationProblemResults(
-        1.0,
+        base_power,
         timestamp_vec,
         data,
         uuid,
@@ -70,6 +70,15 @@ const IS = InfrastructureSystems
     var_res = read_variable(opt_res1, var_key)
     @test size(var_res) == (2, 2)
     @test length(var_res[!, "DateTime"]) == 2
+    @test [1.0, 2.0] == var_res[!, 2]
+    IS.Optimization.convert_result_to_natural_units(::Type{<:IS.Optimization.MockVariable}) = true
+    var_res = IS.Optimization.read_variable(opt_res1, var_key)
+    @test var_res[!, 2] == [10.0, 20.0]
     # Check that expression only has a single column
-    @test size(read_expression(opt_res2, exp_key)) == (1, 1)
+    exp_res = read_expression(opt_res2, exp_key)
+    @test size(exp_res) == (1, 1)
+    @test exp_res[!, 1] == 1.0
+    IS.Optimization.convert_result_to_natural_units(::Type{<:IS.Optimization.MockExpression}) = true
+    exp_res = read_expression(opt_res2, exp_key)
+    @test exp_res[!, 1] == 10.0
 end
