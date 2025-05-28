2f7e4821656d283f6232a834ca0db0fb6ca1a4df
Daniel Thom <daniel.thom@nrel.gov>
2025-05-28 14:32:43 -0600

    Add get_component_supplemental_attribute_pairs

--- src/system_data.jl
+++ src/system_data.jl
@@ -1142,6 +1142,34 @@ function get_available_associated_components(
     return components
 end
 
+"""
+Return a vector of NamedTuples with pairs of components and supplemental attributes that
+are associated with each other.
+"""
+function get_component_supplemental_attribute_pairs(
+    ::Type{T},
+    ::Type{U},
+    data::SystemData;
+    only_available_components::Bool = false,
+) where {T <: InfrastructureSystemsComponent, U <: SupplementalAttribute}
+    ca_pairs = NamedTuple{(:component, :supplemental_attribute), Tuple{T, U}}[]
+    for (component_uuid, attribute_uuid) in
+        list_associated_pair_uuids(
+        data.supplemental_attribute_manager.associations,
+        T,
+        U,
+    )
+        component = get_component(data, component_uuid)
+        if only_available_components && !get_available(component)
+            continue
+        end
+        attribute = get_supplemental_attribute(data, attribute_uuid)
+        push!(ca_pairs, (component = component, supplemental_attribute = attribute))
+    end
+
+    return ca_pairs
+end
+
 function get_masked_components(
     ::Type{T},
     data::SystemData,
