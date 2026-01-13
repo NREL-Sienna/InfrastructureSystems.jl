637cc66f8ff540acd6dd9460a8a9595e69442ee5
Youssef <47382186+ymiftah@users.noreply.github.com>
2026-01-14 04:35:46 +1100

    fixes for 1.13 (#527)
    * fixes for 1.13
    
    * split methods w + wo state

--- src/utils/flatten_iterator_wrapper.jl
+++ src/utils/flatten_iterator_wrapper.jl
@@ -14,7 +14,13 @@ end
 
 Base.@propagate_inbounds function Base.iterate(
     iter::FlattenIteratorWrapper{T, I},
-    state = (),
+) where {T, I}
+    Base.iterate(iter.iter)
+end
+
+Base.@propagate_inbounds function Base.iterate(
+    iter::FlattenIteratorWrapper{T, I},
+    state,
 ) where {T, I}
     Base.iterate(iter.iter, state)
 end
