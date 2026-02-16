76dacd7318dd020a24c6c80c9175eb88a6cdb086
Luke Kiernan <lkiernan@nrel.gov>
2026-02-16 09:42:23 -0700

    add test for bug

--- test/test_assert_op.jl
+++ test/test_assert_op.jl
@@ -8,3 +8,15 @@
     @test_throws AssertionError IS.@assert_op a + 3 == b + 2
     @test_throws AssertionError IS.@assert_op isequal(a + 3, b + 2)
 end
+
+abstract type MyType end
+struct SubtypeOne <: MyType end
+struct SubtypeTwo <: MyType end
+
+@testset "Test assert_op with custom types" begin
+    @noinline function type_param_fcn(::T) where {T <: MyType}
+        IS.@assert_op T <: SubtypeOne
+    end
+    type_param_fcn(SubtypeOne())
+    @test_throws AssertionError type_param_fcn(SubtypeTwo())
+end
